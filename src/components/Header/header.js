dd670c33f72effe1ccb149d3bf9f2b19b795c694
byiringiroscar <byiringoroscar@gmail.com>
2023-04-15 18:48:09 +0200

    Style header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -4,10 +4,13 @@ import { Link } from 'react-router-dom';
 const Header = () => (
   <header>
     <Link to="/" className="logo">Book store</Link>
-    <ul>
+    <ul className="nav-links">
       <li><Link to="/" className="nav-link">Home</Link></li>
       <li><Link to="/categories" className="nav-link">Categories</Link></li>
     </ul>
+    <button type="button" className="icon-button">
+      <span className="material-icons primary-color">person</span>
+    </button>
   </header>
 );
 
